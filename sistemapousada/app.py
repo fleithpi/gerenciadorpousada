b45f4a5aa896c6597a5b6849f0dee07b483a6adc
Pietro Caetano Fleith <ceco1995@gmail.com>
2026-06-12 16:47:23 -0300

    Update app.py

--- sistemapousada/app.py
+++ sistemapousada/app.py
@@ -1,4 +1,6 @@
 import os
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from sistemapousada.models import Usuario 
 from flask import Flask, render_template, request, redirect, flash, url_for, send_from_directory, abort
 from datetime import datetime, date
 from functools import wraps
