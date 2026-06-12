576154da65b3137231075a470c10c5478b92d5bd
Pietro Caetano Fleith <ceco1995@gmail.com>
2026-06-12 16:45:21 -0300

    Update models.py
    Incluindo autenticação com sha256, seguro contra brute force,

--- sistemapousada/models.py
+++ sistemapousada/models.py
@@ -1,5 +1,18 @@
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 from sistemapousada.database import db
 
+class Usuario(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password, method='pbkdf2:sha256')
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+                              
 class pousada(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
