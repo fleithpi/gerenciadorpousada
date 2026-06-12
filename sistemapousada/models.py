a0f19a0c12c530065b55c0d8a8647a6b6606d6e7
Pietro Caetano Fleith <ceco1995@gmail.com>
2026-06-12 17:06:34 -0300

    Update models.py

--- sistemapousada/models.py
+++ sistemapousada/models.py
@@ -5,7 +5,7 @@ from sistemapousada.database import db
 class Usuario(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(200)), nullable=False
+    password_hash = db.Column(db.String(200)), nullable==False
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password, method='pbkdf2:sha256')
