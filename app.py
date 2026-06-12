fc7aac7272a3674e6a1d8c33985d0637b106b20e
Pietro Caetano Fleith <ceco1995@gmail.com>
2026-06-12 15:12:10 -0300

    Update app.py
    alterando o app.py na raiz, adcionando import sys para corrigir o erro

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import os
 import importlib.util
+import sys
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 APP_DIR = os.path.join(BASE_DIR, 'sistemapousada')
+
+sys.path.insert(0, APP_DIR)
 
 spec = importlib.util.spec_from_file_location('sistemapousada_app', os.path.join(APP_DIR, 'app.py'))
 app_module = importlib.util.module_from_spec(spec)
